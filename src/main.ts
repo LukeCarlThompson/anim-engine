fa8675330047cf4038346bfabfd5cc7fe135e1c8
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-11-21 23:13:05 +0800

    create a demo

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,29 @@
 import "./style.css";
 
-export const placeholder = "placeholder";
+import { createAnimEngine, getTicker } from "./anim-engine";
+
+const block: HTMLDivElement | null = document.querySelector(".block");
+const appElement: HTMLDivElement | null = document.querySelector("#app");
+
+if (!block || !appElement) {
+  throw new Error("unable to find required elements");
+}
+
+const ticker = getTicker();
+ticker.autoStart = true;
+
+const animation = createAnimEngine({
+  from: 0,
+  to: 200,
+  durationMs: 2000,
+  ease: "outQuart",
+  onUpdate: (value: number) => {
+    block.style.transform = `translateX(${value}px)`;
+  },
+});
+
+appElement.addEventListener("click", (e) => {
+  animation.to = e.clientX;
+  animation.from = animation.currentValue;
+  void animation.play();
+});
