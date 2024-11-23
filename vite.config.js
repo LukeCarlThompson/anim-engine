a3f700ef13334a9f413efccbcff8aba0ef3b5f87
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-11-24 03:45:02 +0800

    remove vite plugin eslint

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
 import { defineConfig } from "vite";
-import eslint from "vite-plugin-eslint";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [eslint()],
+  plugins: [],
 });
