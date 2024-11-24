ca81f6d23fcb6d623504b31047d243173d5c92bd
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-11-24 16:28:08 +0800

    installd vite dts and configure vite to build dist

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
+import { join, resolve } from "node:path";
+
 import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [],
+  plugins: [dts({ tsconfigPath: "tsconfig.build.json" })],
+  build: {
+    copyPublicDir: false,
+    lib: {
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+      entry: resolve(join(import.meta.dirname, "src/anim-engine/index.ts")),
+      formats: ["es"],
+      fileName: "index.js",
+    },
+  },
 });
