f5758e8b2b51791f9f299298157b9dd64edd9800
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-11-24 18:55:24 +0800

    add in dynamic values story

---
+++ src/anim-engine/implementation/anim-engine-dynamic-values.stories.ts
@@ -0,0 +1,111 @@
+import type { Meta, StoryObj } from "@storybook/html";
+import { fn } from "@storybook/test";
+import { Pane } from "tweakpane";
+import { EASE_NAMES } from "./easing";
+
+import { createAnimEngine } from "../create-anim-engine";
+import { addTweakpaneControls } from "./add-controls.tweakpane";
+
+import type { AnimEngineOptions } from "../anim-engine";
+
+const meta = {
+  title: "Example/AnimEngine function value",
+  tags: ["autodocs"],
+  render: ({ ease, durationMs, onEnded, onStarted, onRepeat }) => {
+    const container = document.createElement("div");
+    container.style.position = "relative";
+    container.style.height = "100vh";
+    container.style.width = "100%";
+    container.style.display = "flex";
+    container.style.alignItems = "center";
+
+    const target = document.createElement("div");
+    target.style.height = "40px";
+    target.style.width = "40px";
+    target.style.left = "200px";
+    target.style.backgroundColor = "red";
+    target.style.borderRadius = "100px";
+    target.style.position = "absolute";
+    target.style.zIndex = "1";
+    target.style.cursor = "grab";
+    container.appendChild(target);
+
+    let isGrabbing = false;
+    target.addEventListener("mousedown", () => {
+      isGrabbing = true;
+    });
+
+    container.addEventListener("mouseup", () => {
+      isGrabbing = false;
+    });
+
+    container.addEventListener("mousemove", (e) => {
+      if (!isGrabbing) return;
+      // const topOffset = e.clientY - container.getBoundingClientRect().top - 20;
+      const leftOffset = e.clientX - container.getBoundingClientRect().left - 20;
+      // target.style.top = `${topOffset}px`;
+      target.style.left = `${leftOffset}px`;
+    });
+
+    const block = document.createElement("div");
+    block.style.height = "100px";
+    block.style.width = "100px";
+    block.style.backgroundColor = "pink";
+
+    container.appendChild(block);
+
+    const animEngine = createAnimEngine({
+      from: () => {
+        return block.getBoundingClientRect().left - container.getBoundingClientRect().left;
+      },
+      to: () => {
+        return target.getBoundingClientRect().left - container.getBoundingClientRect().left - 30;
+      },
+      ease,
+      durationMs,
+      onStarted,
+      onRepeat,
+      onEnded,
+      onUpdate: (value, velocity) => {
+        // TODO: Use the array of values to set the x and y coords of the block when that feature has been implemented.
+        block.style.transform = `translateX(${value}px) rotate(${velocity}deg)`;
+      },
+    });
+
+    const tweakpane = new Pane();
+    tweakpane.element.style.position = "absolute";
+    tweakpane.element.style.top = "10px";
+    tweakpane.element.style.right = "10px";
+
+    container.appendChild(tweakpane.element);
+
+    addTweakpaneControls(animEngine, tweakpane);
+
+    return container;
+  },
+  parameters: {
+    layout: "fullscreen",
+  },
+  argTypes: {
+    durationMs: { control: "number" },
+    ease: {
+      control: "select",
+      options: EASE_NAMES,
+    },
+  },
+} satisfies Meta<AnimEngineOptions>;
+
+export default meta;
+type Story = StoryObj<AnimEngineOptions>;
+
+export const Primary: Story = {
+  name: "Anim Engine",
+  args: {
+    durationMs: 1000,
+    ease: "inOutQuad",
+    repeat: 0,
+    onStarted: fn(),
+    onEnded: fn(),
+    onRepeat: fn(),
+  },
+};
