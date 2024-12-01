68d24afc8617c2195a5506ff8b884c95e254a2b9
Luke Carl Thompson <lukecarlthompson@gmail.com>
2024-12-01 13:36:16 +0800

    clean up

--- src/anim-engine/implementation/index.ts
+++ src/anim-engine/implementation/index.ts
@@ -2,4 +2,3 @@ export { AnimEngine } from "./anim-engine";
 export type { AnimEngineInternalApi } from "./anim-engine";
 export { AnimSequence } from "./anim-sequence";
 export { EASE_NAMES } from "./easing";
-export { Ticker } from "./ticker";
