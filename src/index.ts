ff1a92c50dd305b25163f610af90520f30bc87b9
Luke Carl Thompson <lukecarlthompson@gmail.com>
2026-07-02 22:23:43 +0800

    export smooth damp and lerp

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,8 @@ export { animate } from "./tween/create-tween";
 export { createTimeline } from "./timeline/create-timeline";
 export type { Timeline } from "./timeline/create-timeline";
 export { createSpring } from "./spring/create-spring";
-// export { createSmoothDamp } from "./smooth-damp/create-smooth-damp";
-// export { createLerp } from "./lerp/create-lerp";
+export { createSmoothDamp } from "./smooth-damp/create-smooth-damp";
+export { createLerp } from "./lerp/create-lerp";
 
 // Ticker
 export { getTicker, createTicker } from "./ticker/get-ticker";
@@ -26,6 +26,7 @@ export type {
   TimelineHandle,
   TimelineOptions,
   TickerControls,
+  DynamicValue,
   SpringOptions,
   SmoothDampOptions,
   LerpOptions,
