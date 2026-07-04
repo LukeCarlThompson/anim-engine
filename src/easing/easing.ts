bd29ee832e89ad9356a096a3b8816a899deb0202
Luke Carl Thompson <lukecarlthompson@gmail.com>
2026-07-04 17:57:57 +0800

    optimise easings

--- src/easing/easing.ts
+++ src/easing/easing.ts
@@ -70,16 +70,28 @@ export const easingFunctions: Record<EaseName, EaseFunction> = {
   inOutQuad: (x) => (x < 0.5 ? 2 * x * x : 1 - pow(-2 * x + 2, 2) / 2),
 
   inCubic: (x) => x * x * x,
-  outCubic: (x) => 1 - pow(1 - x, 3),
-  inOutCubic: (x) => (x < 0.5 ? 4 * x * x * x : 1 - pow(-2 * x + 2, 3) / 2),
+  outCubic: (x) => { const t = 1 - x; return 1 - t * t * t; },
+  inOutCubic: (x) => {
+    if (x < 0.5) return 4 * x * x * x;
+    const t = -2 * x + 2;
+    return 1 - t * t * t / 2;
+  },
 
   inQuart: (x) => x * x * x * x,
-  outQuart: (x) => 1 - pow(1 - x, 4),
-  inOutQuart: (x) => (x < 0.5 ? 8 * x * x * x * x : 1 - pow(-2 * x + 2, 4) / 2),
+  outQuart: (x) => { const t = 1 - x; const t2 = t * t; return 1 - t2 * t2; },
+  inOutQuart: (x) => {
+    if (x < 0.5) return 8 * x * x * x * x;
+    const t = -2 * x + 2;
+    return 1 - (t * t * t * t) / 2;
+  },
 
   inQuint: (x) => x * x * x * x * x,
-  outQuint: (x) => 1 - pow(1 - x, 5),
-  inOutQuint: (x) => (x < 0.5 ? 16 * x * x * x * x * x : 1 - pow(-2 * x + 2, 5) / 2),
+  outQuint: (x) => { const t = 1 - x; const t2 = t * t; return 1 - t2 * t2 * t; },
+  inOutQuint: (x) => {
+    if (x < 0.5) return 16 * x * x * x * x * x;
+    const t = -2 * x + 2;
+    return 1 - (t * t * t * t * t) / 2;
+  },
 
   inSine: (x) => 1 - cos((x * PI) / 2),
   outSine: (x) => sin((x * PI) / 2),
@@ -90,17 +102,17 @@ export const easingFunctions: Record<EaseName, EaseFunction> = {
   inOutExpo: (x) =>
     x === 0 ? 0 : x === 1 ? 1 : x < 0.5 ? pow(2, 20 * x - 10) / 2 : (2 - pow(2, -20 * x + 10)) / 2,
 
-  inCirc: (x) => 1 - sqrt(1 - pow(x, 2)),
-  outCirc: (x) => sqrt(1 - pow(x - 1, 2)),
+  inCirc: (x) => 1 - sqrt(1 - x * x),
+  outCirc: (x) => sqrt(1 - (x - 1) * (x - 1)),
   inOutCirc: (x) =>
-    x < 0.5 ? (1 - sqrt(1 - pow(2 * x, 2))) / 2 : (sqrt(1 - pow(-2 * x + 2, 2)) + 1) / 2,
+    x < 0.5 ? (1 - sqrt(1 - 4 * x * x)) / 2 : (sqrt(1 - 4 * (x - 1) * (x - 1)) + 1) / 2,
 
   inBack: (x) => c3 * x * x * x - c1 * x * x,
-  outBack: (x) => 1 + c3 * pow(x - 1, 3) + c1 * pow(x - 1, 2),
+  outBack: (x) => { const t = x - 1; return 1 + c3 * t * t * t + c1 * t * t; },
   inOutBack: (x) =>
     x < 0.5
-      ? (pow(2 * x, 2) * ((c2 + 1) * 2 * x - c2)) / 2
-      : (pow(2 * x - 2, 2) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2,
+      ? (4 * x * x * ((c2 + 1) * 2 * x - c2)) / 2
+      : (4 * (x - 1) * (x - 1) * ((c2 + 1) * (x * 2 - 2) + c2) + 2) / 2,
 
   inElastic: (x) => (x === 0 ? 0 : x === 1 ? 1 : -pow(2, 10 * x - 10) * sin((x * 10 - 10.75) * c4)),
   outElastic: (x) => (x === 0 ? 0 : x === 1 ? 1 : pow(2, -10 * x) * sin((x * 10 - 0.75) * c4) + 1),
